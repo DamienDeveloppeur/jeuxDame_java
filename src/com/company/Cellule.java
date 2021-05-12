b52e2a4a067d3b0b19aca812b2b88429b15212ab
Uncle_scrooge <damienlhaurado@hotmail.com>
2021-05-12 19:45:07 +0200

    placement des pions

--- src/com/company/Cellule.java
+++ src/com/company/Cellule.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,8 +15,8 @@ public class Cellule extends JPanel implements MouseListener {
     Cellule[][] grille = new Cellule [10][10];
     Dimension ech = new Dimension();
 
-    public HashMap<Integer, Integer> pionBlanc = new HashMap<Integer, Integer>();
-    public HashMap<Integer, Integer> pionNoir = new HashMap<Integer, Integer>();
+    public static HashMap<Integer, Integer> pionBlanc = new HashMap<Integer, Integer>();
+    public static HashMap<Integer, Integer> pionNoir = new HashMap<Integer, Integer>();
     public static ArrayList<ArrayList<Integer>> caseValide = new ArrayList<ArrayList<Integer> >();
     static int index = 0;
 
@@ -23,7 +24,7 @@ public class Cellule extends JPanel implements MouseListener {
         addMouseListener(this);
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         for(int x = 0; x < grille.length; x++){
             for(int y=0; y<grille[x].length; y++){
                 grille[x][y] = new Cellule();
@@ -38,38 +39,42 @@ public class Cellule extends JPanel implements MouseListener {
     }
 
     public void dessineToi(Graphics g, int x, int y, Dimension ech) {
-        Image img = null;
+         BufferedImage pionBlancImg = null;
+         BufferedImage pionNoirImg = null;
         try {
-            img = ImageIO.read(new File("data/pionBlanc.png"));
+            pionBlancImg = ImageIO.read(new File("D:\\DEV_Projet_java\\untitled1\\data\\pionBlanc10.png"));
+            pionNoirImg = ImageIO.read(new File("D:\\DEV_Projet_java\\untitled1\\data\\pionNoir10.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
         // on colore les cases
-        if (((x % 2) == 0 && (y % 2) == 0 ) || (x % 2) != 0 && (y % 2) != 0){
-            g.setColor(Color.RED);
-            g.fillRect(x*ech.width, y*ech.height, ech.width, ech.height);
-
-            caseValide.add(new ArrayList<Integer>());
-            caseValide.get(Cellule.index).add(0, y);
-            caseValide.get(Cellule.index).add(1, x);
-
-            System.out.println(Cellule.index);
-           // System.out.println(pionBlanc);
-            if (y >= 6){
-                pionBlanc.put(x,y);
-                g.drawImage(img, x*ech.width, y*ech.height, this.getWidth(), this.getHeight(), this);
-            }
-            if(y <= 3){
-                pionNoir.put(x,y);
-            }
-            Cellule.index ++;
-        }
         g.drawRect(x*ech.width, y*ech.height, ech.width, ech.height);
+
+            if (((x % 2) == 0 && (y % 2) == 0 ) || (x % 2) != 0 && (y % 2) != 0){
+                g.setColor(Color.RED);
+                g.fillRect(x*ech.width, y*ech.height, ech.width, ech.height);
+                if(caseValide.size() < 51){
+                    caseValide.add(new ArrayList<Integer>());
+                    caseValide.get(Cellule.index).add(0, y);
+                    caseValide.get(Cellule.index).add(1, x);
+                }
+                System.out.println(Cellule.index);
+                if (y >= 6){
+                    pionBlanc.put(x,y);
+                    g.drawImage(pionBlancImg,x*ech.width, y*ech.height, ech.width, ech.height,null );
+                }
+                else if(y <= 3 ){
+                    pionNoir.put(x,y);
+                    g.drawImage(pionNoirImg,x*ech.width, y*ech.height, ech.width, ech.height,null );
+                }
+
+            }
+
+
     }
     public void calculerEchelle() {
         ech.width= getWidth()/grille.length;
         ech.height=getHeight()/grille[0].length;
-        grille[4][4].setBackground(Color.BLUE);
     }
 
     public void click() {
@@ -82,8 +87,7 @@ public class Cellule extends JPanel implements MouseListener {
         Point pt = e.getPoint();
         pt.x/=ech.width;
         pt.y/=ech.height;
-        //System.out.println("x : "+pt.x);
-        //System.out.println("y : "+pt.y);
+
         grille[pt.x][pt.y].click();
         repaint();
     }
