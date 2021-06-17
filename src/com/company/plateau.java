08c8b11fe090f251ac829cea17c787493e8c9745
damien <damienlhaurado@hotmail.com>
2021-06-17 15:12:22 +0200

    add jframe for choose play agains't human or bot

--- src/com/company/plateau.java
+++ src/com/company/plateau.java
@@ -9,6 +9,17 @@ public class plateau extends JFrame  {
     private JPanel redPan=new JPanel();
     public JFrame frame = new JFrame();;
     public plateau(){
+        JFrame f=new JFrame("Button Example");
+        JButton b1=new JButton("Play agains't human ?");
+        JButton b2=new JButton("Play agains't bot ?");
+        b1.setBounds(50,10,200,30);
+        b2.setBounds(50,100,200,30);
+        f.add(b1);
+        f.add(b2);
+        f.setSize(300,200);
+        f.setLayout(null);
+        f.setVisible(true);
+/*
         Cellule Cellule = new Cellule(frame);
         Cellule.validate();
         this.setTitle("Jeux de dame");
@@ -27,6 +38,7 @@ public class plateau extends JFrame  {
         this.setContentPane(Cellule);
         SwingUtilities.updateComponentTreeUI(frame);
         this.setVisible(true);
+        */
     }
 
 }
