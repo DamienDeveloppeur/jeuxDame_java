a627c3f6d70a1a58d2566093533faa7c5cf6bf81
merovingien <damienlhaurado@hotmail.com>
2023-10-03 22:22:02 +0200

    add stackoverflow documentation

--- src/test/java/en/queengame/QueenTest.java
+++ src/test/java/en/queengame/QueenTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import queengame.Piece;
@@ -19,25 +20,38 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class QueenTest {
-
+    @Spy
     @InjectMocks
     private Queen sut;
 
-    @Mock
-    private Piece queenMock;
+    @Spy
+    private Queen queenMock;
 
     @BeforeEach
     void setUp() {
-        sut = new Queen(4,6,true);
+        //sut = new Queen(4,6,true);
+        sut.setX(4);
+        sut.setY(6);
+        sut.setColor(true);
     }
 
     @Test
     void testTryingMooveQueen(){
-        ValidCell validCell = new ValidCell(0,2);
+        ValidCell validCell = new ValidCell(2,4);
 
         sut.tryingMoove(validCell);
-        verify(queenMock, times(1))
-                .moove(any(ValidCell.class), anyBoolean());
+        //https://stackoverflow.com/questions/30774358/how-can-i-mock-methods-of-injectmocks-class
+        verify(sut, times(1))
+                .moove(validCell, true);
     }
-
 }
+
+//    @Test
+//    void testTryingMooveQueen2(){
+//        ValidCell validCell = new ValidCell(0,2);
+//
+//        sut.tryingMoove(validCell);
+//        verify(queenMock,
+//                times(1))
+//                .moove(any(), any());
+//    }
